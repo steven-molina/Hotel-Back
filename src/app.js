ac214ae75fa6fe2d4534cd754984e45d8b893957
Chinomasacre <junioryque2014@gmail.com>
2025-04-01 08:27:14 -0500

    registrarse hecho

---
+++ src/app.js
@@ -0,0 +1,27 @@
+
+require('dotenv').config();
+const express = require("express");
+const rutaUser = require("./routes/userRutas");
+//const rutaHabitacion= require("./routes/habitacionRutas")
+//const rutaResena = require("./routes/resenaRutas")
+//const rutaEmpleado = require("./routes/empleadoRutas")
+//const  rutaReserva=require("./routes/reservasRutas")
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const app = express();
+app.use(cors({
+    origin:"http://localhost:3000",
+    credentials:true
+}));
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false })); //entiende datos de un formulario
+app.use("/api", rutaUser);
+//app.use("/api", rutaHabitacion);
+//app.use("/api", rutaResena);
+//app.use("/api", rutaEmpleado);
+//app.use("/api", rutaReserva);
+app.use(require("./middlewares/Errores").ManejoError);
+module.exports = app;
--- src/libs/CrearToken.js
+++ src/libs/CrearToken.js
@@ -1,9 +1,14 @@
+// src/libs/CrearToken.js
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
-
 dotenv.config();
-const CrearToken =  (userId) => {
-  const token = jwt.sign({ id: userId }, process.env.JWT_SECRET); //codificando token
-  return token;
+
+const CrearToken = (userId) => {
+  return jwt.sign(
+    { id: userId },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  );
 };
-module.exports = { CrearToken };
+
+module.exports = { CrearToken }; //Exporta como objeto
