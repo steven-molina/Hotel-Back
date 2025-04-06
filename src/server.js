4b78542caa8057394101aefe1643c76b41ce027f
Chinomasacre <junioryque2014@gmail.com>
2025-04-06 06:15:12 -0500

    correccion de despliegue

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ const app = require("./app");
 require("./database/db");
 async function init() {
   await app.listen(4000);
-  console.log("servidor corriendo en: http://localhost:4000");
+  console.log("servidor corriendo en: "+process.env.MONGODB_URI);
 }
 init();
